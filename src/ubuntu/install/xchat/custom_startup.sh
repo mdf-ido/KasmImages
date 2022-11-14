dc02db4a8675631a0ed4de66eb75b0feef16b63d
Mo Figueroa <moisesdfigueroa@gmail.com>
2022-11-13 22:33:18 -0500

    Updating image
    Signed-off-by: Mo Figueroa <moisesdfigueroa@gmail.com>

---
+++ src/ubuntu/install/xchat/custom_startup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -ex
+FORCE=$1
+
+if ( [ -z "$DISABLE_CUSTOM_STARTUP" ] ||  [ -n "$FORCE" ] ) ; then
+    if [ -f /tmp/custom_startup.lck ] ; then
+        echo "custom_startup already running!"
+        exit 1
+    fi
+    touch /tmp/custom_startup.lck
+    while true
+    do
+        if ! pgrep -x xchat > /dev/null
+        then
+            cd $HOME
+            /usr/bin/desktop_ready
+            xchat $ARGS
+        fi
+        sleep 1
+    done
+    rm /tmp/custom_startup.lck
+fi
